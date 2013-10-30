df2164b480d3433004ac95cfdad508a0cfbd3a93
Leongum <luyongluy@gmail.com>
2013-10-30 16:07:04 +0800

    commit SNS login

--- src/com/G5432/Cyberace/Main/LoginActivity.java
+++ src/com/G5432/Cyberace/Main/LoginActivity.java
@@ -4,11 +4,20 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.*;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.renren.Renren;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.tencent.qzone.QZone;
+import cn.sharesdk.tencent.weibo.TencentWeibo;
 import com.G5432.Cyberace.R;
 import com.G5432.Cyberace.Setting.BodySettingActivity;
 import com.G5432.DBUtils.DatabaseHelper;
@@ -27,6 +36,7 @@ import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import java.text.MessageFormat;
+import java.util.HashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -47,11 +57,41 @@ public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
     private TextView txtEmail;
     private SystemService systemService;
     private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+    private RelativeLayout layoutSNSs;
+    private Button btnQQ;
+    private Button btnWeibo;
+    private Button btnTT;
+    private Button btnRenRen;
+
+    // 定义Handler
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Platform plat = (Platform) msg.obj;
+            switch (msg.arg1) {
+                case 1: {
+                    // 成功
+                    doSNSLogin(plat);
+                }
+                break;
+                case 2: {
+                    // 失败
+                    CommonUtil.showMessages(getApplicationContext(), "呃。授权失败了～");
+                }
+                case 3: {
+                    // 取消
+                    CommonUtil.showMessages(getApplicationContext(), "取消了授权？");
+                }
+            }
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        ShareSDK.initSDK(this);
         btnLogin = (Button) findViewById(R.id.loginBtnLogin);
         btnRegister = (Button) findViewById(R.id.loginBtnResigter);
         btnSubmit = (Button) findViewById(R.id.loginBtnSubmit);
@@ -60,6 +100,11 @@ public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
         txtPassword = (TextView) findViewById(R.id.loginTxtPassword);
         txtEmail = (TextView) findViewById(R.id.loginTxtUserEmail);
         txtNickName = (TextView) findViewById(R.id.loginTxtNickName);
+        layoutSNSs = (RelativeLayout) findViewById(R.id.loginLayoutSNSs);
+        btnQQ = (Button) findViewById(R.id.loginBtnQQ);
+        btnWeibo = (Button) findViewById(R.id.loginBtnSina);
+        btnTT = (Button) findViewById(R.id.loginBtnTT);
+        btnRenRen = (Button) findViewById(R.id.loginBtnRenRen);
         systemService = new SystemService(getHelper());
         initPage();
     }
@@ -140,6 +185,75 @@ public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
         }
     };
 
+    public void doSNSLogin(Platform platform){
+        final HttpClientHelper httpClientHelper = new HttpClientHelper();
+        final String userName = platform.getDb().getUserId();
+        final String password = CommonUtil.getMD5(userName);
+        final String nickName = platform.getDb().getUserName();
+        String url = MessageFormat.format(Constant.LOGIN_URL, userName, password);
+        httpClientHelper.get(CommonUtil.getUrl(url), null, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, String response) {
+                Log.d(this.getClass().getName(), response);
+                if (statusCode == 200 || statusCode == 204) {
+                    UserService userService = new UserService(getHelper());
+                    UserBase userBase = gson.fromJson(response, UserBase.class);
+                    UserInfo userInfo = gson.fromJson(response, UserInfo.class);
+                    userBase.setUserInfo(userInfo);
+                    userBase.setNickName(nickName);
+                    userService.saveUserInfoToDB(userBase);
+                    UserUtil.saveUserInfoToList(userBase);
+                    BackendSyncRequest backendSyncRequest = new BackendSyncRequest(getHelper());
+                    backendSyncRequest.syncRunningHistories();
+                    backendSyncRequest.uploadRunningHistories();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Log.e(this.getClass().getName(), response);
+                    String message = systemService.getSystemMessage(Constant.LOGIN_ERROR);
+                    CommonUtil.showMessages(getApplicationContext(), message);
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable error, String content) {
+                UserBase userBase = new UserBase();
+                userBase.setNickName(nickName);
+                userBase.setUserEmail(userName);
+                userBase.setPassword(password);
+                String requestBody = gson.toJson(userBase, UserBase.class);
+                httpClientHelper.post(CommonUtil.getUrl(Constant.REGISTER_URL), null, requestBody, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, String response) {
+                        Log.d(this.getClass().getName(), response);
+                        if (statusCode == 200 || statusCode == 204) {
+                            UserService userService = new UserService(getHelper());
+                            UserBase userBase = gson.fromJson(response, UserBase.class);
+                            UserInfo userInfo = gson.fromJson(response, UserInfo.class);
+                            userBase.setUserInfo(userInfo);
+                            userService.saveUserInfoToDB(userBase);
+                            UserUtil.saveUserInfoToList(userBase);
+                            Intent intent = new Intent(LoginActivity.this, BodySettingActivity.class);
+                            intent.putExtra("NewRegister", true);
+                            startActivity(intent);
+                        } else {
+                            Log.e(this.getClass().getName(), response);
+                            String message = systemService.getSystemMessage(Constant.REGISTER_FAIL);
+                            CommonUtil.showMessages(getApplicationContext(), message);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Throwable error, String content) {
+                        String message = systemService.getSystemMessage(Constant.REGISTER_FAIL);
+                        CommonUtil.showMessages(getApplicationContext(), message);
+                        Log.e(this.getClass().getName(), error.getMessage() + content);
+                    }
+                });
+            }
+        });
+    }
+
 
     private OnClickListener loginSubmitLister = new OnClickListener() {
         @Override
@@ -187,7 +301,8 @@ public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
     private OnClickListener registerListener = new OnClickListener() {
         @Override
         public void onClick(View view) {
-            txtNickName.setAlpha(1);
+            txtNickName.setVisibility(View.VISIBLE);
+            layoutSNSs.setVisibility(View.GONE);
             Resources resources = getResources();
             Drawable btnDrawable = resources.getDrawable(R.drawable.seg_selected_bg);
             btnRegister.setBackgroundDrawable(btnDrawable);
@@ -199,7 +314,8 @@ public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
     private OnClickListener loginListener = new OnClickListener() {
         @Override
         public void onClick(View view) {
-            txtNickName.setAlpha(0);
+            txtNickName.setVisibility(View.GONE);
+            layoutSNSs.setVisibility(View.VISIBLE);
             Resources resources = getResources();
             Drawable btnDrawable = resources.getDrawable(R.drawable.seg_selected_bg);
             btnLogin.setBackgroundDrawable(btnDrawable);
@@ -208,11 +324,70 @@ public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
         }
     };
 
+    PlatformActionListener platformActionListener = new PlatformActionListener() {
+        @Override
+        public void onComplete(Platform platform, int action, HashMap<String, Object> stringObjectHashMap) {
+            Message msg = new Message();
+            msg.arg1 = 1;
+            msg.obj = platform;
+            handler.sendMessage(msg);
+        }
+
+        @Override
+        public void onError(Platform platform, int action, Throwable throwable) {
+            Message msg = new Message();
+            msg.arg1 = 2;
+            msg.obj = platform;
+            handler.sendMessage(msg);
+        }
+
+        @Override
+        public void onCancel(Platform platform, int action) {
+            Message msg = new Message();
+            msg.arg1 = 3;
+            msg.obj = platform;
+            handler.sendMessage(msg);
+        }
+    };
+
+    private void login(String platform) {
+        Platform plat = ShareSDK.getPlatform(this, platform);
+        plat.setPlatformActionListener(platformActionListener);
+        plat.authorize();
+    }
+
     private void initPage() {
         checkShowPassword.setOnCheckedChangeListener(showPasswordListener);
         btnReturn.setOnClickListener(returnListener);
         btnSubmit.setOnClickListener(loginSubmitLister);
         btnRegister.setOnClickListener(registerListener);
         btnLogin.setOnClickListener(loginListener);
+        btnQQ.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(QZone.NAME);
+            }
+        });
+
+        btnRenRen.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(Renren.NAME);
+            }
+        });
+
+        btnTT.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(TencentWeibo.NAME);
+            }
+        });
+
+        btnWeibo.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(SinaWeibo.NAME);
+            }
+        });
     }
 }
