4d080f7bc8bd4fb371cebbe4289274e95a7845bd
Beyond <byzhao@cyberace.cc>
2013-11-01 15:28:34 +0800

    motion cal test

--- src/com/G5432/Cyberace/Run/RunningBasic.java
+++ src/com/G5432/Cyberace/Run/RunningBasic.java
@@ -10,9 +10,10 @@ import android.os.Message;
 import android.widget.TextView;
 import cn.sharesdk.framework.Platform;
 import com.G5432.Cyberace.R;
-import com.G5432.StepCounting.SCCommon;
+import com.G5432.StepCounting.*;
 import com.G5432.Utils.CommonUtil;
 
+import java.text.MessageFormat;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Handler;
@@ -27,36 +28,34 @@ import static java.lang.String.*;
  * Time: 下午2:24
  * To change this template use File | Settings | File Templates.
  */
-public class RunningBasic extends Activity implements SensorEventListener{
-    private SensorManager mySensorManager;
-    private Sensor mySensor;
-    private SensorEvent mySensorEvent;
+public class RunningBasic extends Activity {
+    private AccSensorService accSensorService;
+    private OrientationSensorService orientationSensorService;
     private Timer timer;
 
     // 定义Handler
     android.os.Handler handler = new android.os.Handler() {
         @Override
         public void handleMessage(Message msg) {
-            TextView textView = (TextView)findViewById(R.id.gAccReading);
+            TextView textView = (TextView) findViewById(R.id.gAccReading);
             textView.setText(msg.obj.toString());
         }
     };
 
-    private void initSensors(){
-        mySensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    }
 
     protected void onCreate(android.os.Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.running_normal);
-        initSensors();
+        accSensorService = new AccSensorService(this);
+        orientationSensorService = new OrientationSensorService(this);
+        accSensorService.start();
+        orientationSensorService.start();
     }
 
     protected void onResume() {
         super.onResume();
-        mySensor = mySensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mySensorManager.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_FASTEST);
-        timer=new Timer();
+
+        timer = new Timer();
                 /*定义计划任务，根据参数的不同可以完成以下种类的工作：
                  * 在固定时间执行某任务，在固定时间开始重复执行某任务，
                  * 重复时间间隔可控，在延迟多久后执行某任务，在延迟多久
@@ -69,27 +68,22 @@ public class RunningBasic extends Activity implements SensorEventListener{
                 // TODO Auto-generated method stub
 //                      Message message=new Message();
 //                      message.what=i--;
-                Message msg = new Message();
-                msg.obj =  valueOf(SCCommon.calculateVectorMod(mySensorEvent.values));
-                handler.sendMessage(msg);
 
+                solveDeviceStatus();
             }
-        }, 1000,50);
+        }, 1000, 50);
     }
 
     protected void onStop() {
-        mySensorManager.unregisterListener(this);
         super.onStop();
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        //To change body of implemented methods use File | Settings | File Templates.
-        mySensorEvent = sensorEvent;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    private void solveDeviceStatus() {
+        float R[] = new float[9];
+        SensorManager.getRotationMatrix(R, null, accSensorService.getOriginAcc().v, orientationSensorService.getOriginGeomagnetic().v);
+        DeviceStatus newStatus = new DeviceStatus(accSensorService.getOriginAcc(), R);
+        Message msg = new Message();
+        msg.obj = MessageFormat.format("{0}\n{1}\n{2}", newStatus.getRealAcc().v[0], newStatus.getRealAcc().v[1], newStatus.getRealAcc().v[2]);
+        handler.sendMessage(msg);
     }
 }
